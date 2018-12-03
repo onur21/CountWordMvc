d8bc74e9ac59cab4796a2145108ab286b9224fea
Onur Özdemir <onurrozdemir94@gmail.com>
2018-12-03 22:16:00 +0300

    1

---
+++ Controllers/WordController.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WordsinMobyDick.Controllers
+{
+    public class WordController : Controller
+    {
+        //
+        // GET: /ProductList/
+        public ActionResult Index()
+        {
+            XMLReader readXML = new XMLReader();
+            var data = readXML.RetrunListOfProducts();
+            return View(data.ToList());  
+            return View();
+        }
+	}
+}
